78f8e544843f59094dc91214c0c73704d534041d
lucaza21 <luis.carrasquilla.z@gmail.com>
2023-05-03 23:02:42 -0500

    creando los HTTP requests

--- server.js
+++ server.js
@@ -23,3 +23,107 @@ app.listen(3001, () => console.log('server corriendo en peurto 3001'))
 // routes
 app.use('/', indexRouter)
 
+// ****************************************************************************************
+
+// DB connection
+const mongoose = require('mongoose')
+mongoose
+    .set('strictQuery', false) // only the fields that are specified in my Schema will be saved in the database
+    .connect('mongodb://localhost:27017/bootcamp')
+    .then(() => console.log('CONECTADO A BBDD bootcamp'))
+    .catch(error => console.error(error));
+
+//CORS
+const cors = require('cors')
+app.use(cors())
+
+let dbs = require('./Models/Bootcamp.model')
+
+// ****************************************************************************************
+//                    USERS
+// ****************************************************************************************
+// get all
+  app.get('/api/allUsers', (req, res) => {
+    dbs.Users
+        .find()
+        .then(allUsers => {
+          return res.status(200).json({
+            count: allUsers.length,
+            url: "http://localhost:3001/api/allUsers",
+            users: allUsers
+          })
+      })
+  })
+
+//get by id
+app.get('/api/user/:user_id', async(req, res) => {
+    const {user_id} = req.params
+    await dbs.Users
+        .findById(user_id)
+        .then(user => res.status(200).json(user))
+})
+
+//get by role
+app.get('/api/user/role/:user_role', async(req, res) => {
+    const {user_role} = req.params
+    await dbs.Users
+        .find({role : user_role})
+        .then(users => res.status(200).json(users))
+})
+
+// update one user's role
+app.put('/api/update/:user_id/:user_role', async(req, res) => {
+  const {user_id} = req.params
+  const {user_role} = req.params 
+
+  dbs.Users.exists({_id:user_id}, async function (err, doc) {
+    if (err){
+        //console.log(err)
+        const name= "user_" + Math.floor(Math.random() * 100)
+        const email= name + "@upc.com"
+        const password= "secret"
+        const role = "normal"
+        const newUser = new dbs.Users({name, email, password, role});
+        await newUser.save();
+        res.status(201).json({'status 201 User created': {name,email,password,role}})
+        //res.send(err)
+    } else{
+        //console.log("Result :", doc) // false
+        //res.send(doc)
+        await dbs.Users
+        .findByIdAndUpdate({_id: user_id}, {role: user_role}, {new: true})
+        .then(response =>  res.status(200).json(response))
+        .catch( error => console.log(error)) 
+
+        }
+    });
+});
+
+// delete one user
+app.delete('/api/user/delete/:user_id', async(req, res) => {
+    const {user_id} = req.params
+  
+    dbs.Users.exists({_id:user_id}, async function (err, doc) {
+      if (err){
+          //console.log(err)
+          res.status(204).send({status: '204 No Content'})
+          //res.send(err)
+      } else{
+          //console.log("Result :", doc) // false
+          //res.send(doc)
+          await dbs.Users.findByIdAndRemove(user_id);
+          res.status(200).json({status: `Elemento con id:${user_id} eliminado`})
+  
+          }
+      });
+  })
+
+
+// get Create User
+app.post('/api/createUser/', async (req, res) => {
+      const {name, email, password, role} = req.body;
+      const newUser = new dbs.Users({name, email, password, role});
+      await newUser.save();
+      res.status(200).json({'user': req.body});
+
+})
